cd8ac5a254ede5937f38529fe142d4a3ac8992d5
Vadim Zavertaluik <zavertaluik.v@gmail.com>
2018-09-28 16:13:50 +0300

    Adding mongo.

---
+++ src/main/java/com/entity/Customer.java
@@ -0,0 +1,33 @@
+package com.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Id;
+
+@Getter
+@Setter
+public class Customer {
+
+    @Id
+    public String id;
+
+    public String firstName;
+    public String lastName;
+
+    public Customer() {
+    }
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%s, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+}
--- src/main/java/com/service/TaskService.java
+++ src/main/java/com/service/TaskService.java
@@ -1,6 +1,8 @@
 package com.service;
 
+import com.entity.Customer;
 import com.entity.Task;
+import com.repository.CustomerRepository;
 import com.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,13 +12,21 @@ import java.util.List;
 @Service
 public class TaskService {
     private final TaskRepository taskRepository;
-
+    private final CustomerRepository customerRepository;
     @Autowired
-    public TaskService(TaskRepository taskRepository) {
+    public TaskService(TaskRepository taskRepository,
+                       CustomerRepository customerRepository) {
         this.taskRepository = taskRepository;
+        this.customerRepository = customerRepository;
     }
 
     public List<Task> getAllTasks() {
+        Customer customer = new Customer();
+        customer.setFirstName("first name");
+        customer.setLastName("second name");
+        customerRepository.save(customer);
+        List<Customer> all = customerRepository.findAll();
+        System.out.println("ALL = "+all);
         return taskRepository.findAll();
     }
 }
